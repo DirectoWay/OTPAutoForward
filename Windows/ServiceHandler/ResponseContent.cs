04891c071953730416dcccb5ee0f1e52b79f9fbf
DirectoWay <X471754@163.com>
2025-07-16 23:20:16 +0800

    Feat: Win 端新增 Response 响应类, 用于返回业务结果

---
+++ Windows/ServiceHandler/ResponseContent.cs
@@ -0,0 +1,44 @@
+﻿namespace OTPAutoForward.ServiceHandler
+{
+    public class ResponseContent
+    {
+        public ResponseContent()
+        {
+        }
+
+        public ResponseContent(bool status)
+        {
+            Status = status;
+        }
+
+        public bool Status { get; set; }
+        public string Code { get; set; }
+
+        public string Message { get; set; }
+
+        public object Data { get; set; }
+
+        public ResponseContent OK()
+        {
+            Status = true;
+            return this;
+        }
+
+        public static ResponseContent Instance => new ResponseContent();
+
+        public ResponseContent OK(string message, object data = null)
+        {
+            Status = true;
+            Message = message;
+            Data = data;
+            return this;
+        }
+
+        public ResponseContent Error(string message = null)
+        {
+            Status = false;
+            Message = message;
+            return this;
+        }
+    }
+}
